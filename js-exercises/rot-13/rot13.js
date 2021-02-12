6f3c5aed6cc0594ed87f7b31d98cbf1eef9ee540
Bhupendra Rao <mbhupendrads@gmail.com>
2021-02-12 11:55:38 +0530

    Solved rot-13 problem

--- js-exercises/rot-13/rot13.js
+++ js-exercises/rot-13/rot13.js
@@ -1,5 +1,32 @@
-function rot13(...args) {
-  return args;
+// function isAlpha(ch) {
+//   return /^[A-Z]$/i.test(ch);
+// }
+
+function rot13(str) {
+  let result = '';
+
+  for (let i = 0; i < str.length; i += 1) {
+    const charCode = str.charCodeAt(i);
+
+    if (
+      (charCode < 65 || charCode > 122)
+            || (charCode > 90 && charCode < 97)
+    ) {
+      result += str[i];
+    } else {
+      let newCharCode = charCode + Math.ceil(13 % 26);
+
+      if (charCode >= 97 && newCharCode > 122) {
+        newCharCode = newCharCode - 122 + 96;
+      }
+      if (charCode <= 90 && newCharCode > 90) {
+        newCharCode = newCharCode - 90 + 64;
+      }
+
+      result += String.fromCharCode(newCharCode);
+    }
+  }
+  return result;
 }
 
 export {
