ea4229476752e6ad255db83bda8f2930a13cda1b
bhupen13au <mbhupendrads@gmail.com>
2021-02-17 10:28:50 +0530

    Refactor of sum-even-args done

--- js-exercises/sum-even-args/sumEvenArgs.js
+++ js-exercises/sum-even-args/sumEvenArgs.js
@@ -1,11 +1,14 @@
-const sumEvenArgs = (...args) => {
-  let sum = 0;
-  args.forEach((value) => {
-    if (value % 2 === 0) {
-      sum += value;
-    }
-  });
-  return sum;
-};
+// const sumEvenArgs = (...args) => {
+//   let sum = 0;
+//   args.forEach((value) => {
+//     if (value % 2 === 0) {
+//       sum += value;
+//     }
+//   });
+//   return sum;
+// };
+
+const sumEvenArgs = (...args) => args.filter((value) => value % 2 === 0)
+  .reduce((acc, value) => acc + value, 0);
 
 export { sumEvenArgs };
